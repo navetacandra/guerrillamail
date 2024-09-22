ee26014a29edd7cfbec1b6ab2b4a6d4d2c6e65f8
navetacandra <naveta.cand@gmail.com>
2024-09-22 10:05:18 +0700

    feat: create simple EventEmitter

---
+++ src/event.js
@@ -0,0 +1,27 @@
+class EventEmitter {
+  #events = {};
+  constructor() {}
+
+  on(eventName='', listener=(data)=>{}) {
+    if(!this.#events[eventName]) {
+      this.#events[eventName] = [];
+    }
+    this.#events[eventName].push(listener);
+  }
+
+  off(eventName='', listener=(data)=>{}) {
+    if(!!this.#events[eventName]) {
+      this.#events[eventName].splice(this.#events[eventName].indexOf(listener), 1);
+    }
+  }
+
+  emit(eventName='', data=undefined) {
+    if(!!this.#events[eventName]) {
+      this.#events[eventName].forEach(listener => {
+        listener(data);
+      });
+    }
+  }
+}
+
+exports.EventEmitter = EventEmitter;
